d167ce784cadf2dcca6cf80c229938612ebb5f2c
Yen Yin Hing <yenyinf287@gmail.com>
2019-05-24 00:36:02 -0700

    combining reducers

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,13 +1,18 @@
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-
+// import reducer from './ShareItemPreview/reducer';
+import ShareItemPreviewReducer from './ShareItemPreview/reducer';
 // @TODO: Import your reducers
 
 const middleware = [];
 
 const store = createStore(
-  combineReducers(/* @TODO: Combine your reducers */),
+  combineReducers({
+    ShareItemPreview: ShareItemPreviewReducer
+  }),
+
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
+// (/* @TODO: Combine your reducers */),
