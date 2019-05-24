cd120af3349dd1736f4c4bc6843a7e9704133054
Yen Yin Hing <yenyinf287@gmail.com>
2019-05-24 00:23:47 -0700

    fix styling for form

--- client/src/components/ShareItemForm/styles.js
+++ client/src/components/ShareItemForm/styles.js
@@ -32,6 +32,16 @@ const styles = theme => ({
     fontWeight: '700',
     marginBottom: '80px',
     width: '69%'
+  },
+  descriptionName: {
+    fontSize: '16px',
+    fontWeight: 400
+  },
+  button: {
+    fontSize: '16px',
+    padding: '8px 24px',
+    width: '30%',
+    marginTop: '30px'
   }
 
   // bmtwnHeaderWrapper: {
