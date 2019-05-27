dae17be225374f3c119f7bcc9c7a72bc206cabe3
Yen Yin Hing <yenyinf287@gmail.com>
2019-05-26 23:06:10 -0700

    attempt to import Query

--- client/src/pages/Profile/ProfileContainer.js
+++ client/src/pages/Profile/ProfileContainer.js
@@ -1,18 +1,25 @@
-
 import React, { Component } from 'react';
 import Profile from './Profile';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-// import FullScreenLoader from '../../components/FullScreenLoader';
 // import { Query } from 'react-apollo';
-// import {  } from '../../apollo/queries';
+// import { ALL_USER_ITEMS_QUERY } from '../../apollo/queries';
+// import FullScreenLoader from '../../components/FullScreenLoader';
 
 class ProfileContainer extends Component {
-    render() {
-      return (
-        <Profile />
-      );
-    }
+  render() {
+    return (
+      <Profile />
+      // <Query query={ALL_USER_ITEMS_QUERY}>
+      //   {({ loading, error, data }) => {
+      //     // if (loading) return <FullScreenLoader inverted />;
+      //     if (loading) return <p>Loading...</p>;
+      //     if (error) return <p>{`Error! ${error.message}`}</p>;
+      //     return <Profile classes={this.props.classes} items={data.items} />;
+      //   }}
+      // </Query>
+    );
   }
-  
-  export default withStyles(styles)(ProfileContainer);
+}
+
+export default withStyles(styles)(ProfileContainer);
