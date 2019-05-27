3d711cac677658040f22ca2cec8599d6b96b495e
Yen Yin Hing <yenyinf287@gmail.com>
2019-05-26 23:04:39 -0700

    add Navigation Bar to Profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -2,14 +2,19 @@ import React from 'react';
 import UserProfile from '../../components/UserProfile/UserProfile';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
+import NavigationBar from '../../components/NavigationBar';
 
 const Profile = ({ classes }) => {
   return (
-    <div className={classes.profileContainer}>
-      {/* <p>
+    <div>
+      <NavigationBar />
+      <div className={classes.profileContainer}>
+        {/* <p>
         This is the profile page located at <code>/profile/:userId</code>.
       </p> */}
-      <UserProfile />
+
+        <UserProfile />
+      </div>
     </div>
   );
 };
