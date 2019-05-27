86c007e4911ef2f448f75a2b17703b06ac28cb5b
Yen Yin Hing <yenyinf287@gmail.com>
2019-05-26 23:31:59 -0700

    delete commented code

--- client/src/pages/Items/Items.js
+++ client/src/pages/Items/Items.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-// import Button from '@material-ui/core/Button';
-// import AppBar from '@material-ui/core/AppBar';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import IconButton from '@material-ui/core/IconButton';
-// import Icon from '@material-ui/core/Icon';
-// import MoreVertIcon from '@material-ui/icons/MoreVert';
-// import { Link } from 'react-router-dom';
-// import bmtwnLogo from '../../images/boomtown.svg';
 import styles from './styles';
-
-// import MenuOptions from '../../components/MenuOptions/MenuOptions';
-// import ItemsContainer from './ItemsContainer';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import CardGrid from '../../components/CardGrid';
 
@@ -20,7 +9,6 @@ const Items = ({ classes, items }) => {
     <div className={classes.ItemContainer}>
       <NavigationBar />
       <CardGrid items={items} />
-
       {/* <div>{showAllItems}</div> */}
     </div>
   );
--- client/src/pages/Items/styles.js
+++ client/src/pages/Items/styles.js
@@ -1,41 +1,6 @@
 const styles = theme => ({
   ItemContainer: {
-    display: 'grid',
-    // gridTemplateColumns: '1fr 1fr 1fr',
-    // gridGap: '30px',
-    // width: '95%',
-    // margin: '20px auto',
     backgroundColor: 'black'
-  },
-  button: {
-    margin: theme.spacing.unit
-  },
-  icon: {
-    margin: theme.spacing.unit * 2,
-    marginRight: theme.spacing.unit
-  },
-  margin: {
-    margin: theme.spacing.unit
-  },
-
-  avatar: {
-    margin: 10
-  },
-  bigAvatar: {
-    width: 60,
-    height: 60
-  },
-
-  shareBtn: {
-    // marginRight: theme.spacing.unit,
-    margin: theme.spacing.unit
-  },
-  navContainer: {
-    display: 'flex'
-  },
-
-  itemLink: {
-    flexBasis: '69%'
   }
 });
 
