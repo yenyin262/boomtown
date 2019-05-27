ec6a68f5a68597414c3dc5c6290a502b9a90e111
Yen Yin Hing <yenyinf287@gmail.com>
2019-05-26 23:00:22 -0700

    delete unnecessary import files

--- client/src/pages/Items/Items.js
+++ client/src/pages/Items/Items.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Icon from '@material-ui/core/Icon';
+// import Button from '@material-ui/core/Button';
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import IconButton from '@material-ui/core/IconButton';
+// import Icon from '@material-ui/core/Icon';
 // import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { Link } from 'react-router-dom';
-import bmtwnLogo from '../../images/boomtown.svg';
+// import { Link } from 'react-router-dom';
+// import bmtwnLogo from '../../images/boomtown.svg';
 import styles from './styles';
-import CardItem from '../../components/CardItem/CardItem';
-import MenuOptions from '../../components/MenuOptions/MenuOptions';
-import ItemsContainer from './ItemsContainer';
+
+// import MenuOptions from '../../components/MenuOptions/MenuOptions';
+// import ItemsContainer from './ItemsContainer';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import CardGrid from '../../components/CardGrid';
 
-const Items = ({ classes, dataItems }) => {
-  const showAllItems = dataItems.map((value, index) => {
-    return <CardItem item={value} key={index} />;
-  });
-
+const Items = ({ classes, items }) => {
   return (
     <div className={classes.ItemContainer}>
       <NavigationBar />
+      <CardGrid items={items} />
 
-      <div>{showAllItems}</div>
+      {/* <div>{showAllItems}</div> */}
     </div>
   );
 };
