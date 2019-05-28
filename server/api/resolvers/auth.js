a7e80ecc73c059893a028a1648822dc60d9472e5
Yen Yin Hing <yenyinf287@gmail.com>
2019-05-28 00:26:06 -0700

    add console.log to test temp

--- server/api/resolvers/auth.js
+++ server/api/resolvers/auth.js
@@ -39,7 +39,7 @@ function generateToken(user, secret) {
   // -------------------------------
 }
 
-module.exports = (app) => {
+module.exports = app => {
   return {
     async signup(parent, args, context) {
       try {
@@ -54,15 +54,15 @@ module.exports = (app) => {
          * and store that instead. The password can be decoded using the original password.
          */
         // @TODO: Use bcrypt to generate a cryptographic hash to conceal the user's password before storing it.
-        const hashedPassword = '';
+        const hashedPassword = ''; //homework
         // -------------------------------
-
+        console.log('here1');
         const user = await context.pgResource.createUser({
           fullname: args.user.fullname,
           email: args.user.email,
           password: hashedPassword
         });
-
+        console.log('here2');
         setCookie({
           tokenName: app.get('JWT_COOKIE_NAME'),
           token: generateToken(user, app.get('JWT_SECRET')),
