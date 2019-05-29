4e910503e58a23ee4359f91d1cffb4c9d9b757c1
Yen Yin Hing <yenyinf287@gmail.com>
2019-05-29 00:08:10 -0700

    delete todo comments and refactor makeexecutableschema

--- server/config/apollo.js
+++ server/config/apollo.js
@@ -3,64 +3,36 @@ const { makeExecutableSchema } = require('graphql-tools'); // A set of utilities
 
 const typeDefs = require('../api/schema');
 let resolvers = require('../api/resolvers');
+const { AuthDirective } = require('../api/custom-directives');
 
 module.exports = ({ app, pgResource }) => {
   resolvers = resolvers(app);
-
-  /**
-   * @TODO: Initialize Apollo Server
-   *
-   * Once you've defined your schema types, it's time to wire up your schema
-   * to your resolving functions. This is Apollo magic, and it's done using
-   * the 'makeExecutableSchema' function provided by the 'graphql-tools' package.
-   *
-   * https://www.apollographql.com/docs/apollo-server/v2/api/graphql-tools.html#makeExecutableSchema
-   */
-
-  // @TODO: Refactor to use 'makeExecutableSchema' to wire up your schema to your resolvers:
-
-  // The type definitions are compiled to an executable GraphQL schema by the makeExecutableSchema function from graphql-tools
-
-  const schema = makeExecutableSchema({ 
+  const schema = makeExecutableSchema({
     typeDefs,
-    resolvers
+    resolvers,
+    schemaDirectives: {
+      auth: AuthDirective
+    }
   });
-  // -------------------------------
 
-  const apolloServer = new ApolloServer({ // create function - using req // context is property of apolloserver
-    context: ({ req }) => { // request object through express & has metadata from the client to the server // ie bunch of cookies 
-      const tokenName = app.get("JWT_COOKIE_NAME")
-      const token = req ? req.cookies[tokenName] : undefined
-      // @TODO: Uncomment this later when we add auth (to be added to Apollo's context)
-     
-      // -------------------------------
+  const apolloServer = new ApolloServer({
+    // create function - using req // context is property of apolloserver
+    context: ({ req }) => {
+      // request object through express & has metadata from the client to the server // ie bunch of cookies
+      const tokenName = app.get('JWT_COOKIE_NAME');
+      const token = req ? req.cookies[tokenName] : undefined;
 
       return {
         req,
         token,
         pgResource // returning in apolo context server
-        /**
-         * @TODO: Provide Apollo context
-         *
-         * When initializing Apollo, we can provide a context object which will be
-         * passed to each resolver function. This is useful because there are a
-         * number of things we'll need to access in every resolver function.
-         *
-         * Above we can see that we are capturing the cookie from the request object,
-         * and retrieving the token. This is important for authentication.
-         *
-         * Refactor this code and supply any additional information (values, methods, objects...etc)
-         * you'll need to use in your resolving functions.
-         */
       };
     },
-    schema // use schema 
+    schema // use schema
   });
 
   apolloServer.applyMiddleware({
     app,
-    // @TODO: Add the CORS_CONFIG from your application configuration
     cors: app.get('CORS_CONFIG')
-    // -------------------------------
   });
 };
