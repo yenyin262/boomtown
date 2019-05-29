a3f927b3afa940f11dfaee397b04ccf6774506d2
Yen Yin Hing <yenyinf287@gmail.com>
2019-05-29 00:37:54 -0700

    change to lower case in shareItemPreview

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,14 +1,13 @@
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-// import reducer from './ShareItemPreview/reducer';
+
 import ShareItemPreviewReducer from './ShareItemPreview/reducer';
-// @TODO: Import your reducers
 
 const middleware = [];
 
 const store = createStore(
   combineReducers({
-    ShareItemPreview: ShareItemPreviewReducer
+    shareItemPreview: ShareItemPreviewReducer
   }),
 
   composeWithDevTools(applyMiddleware(...middleware))
