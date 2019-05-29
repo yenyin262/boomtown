686503d1eb9aeb3f575e2575ce46267a5209275d
Yen Yin Hing <yenyinf287@gmail.com>
2019-05-29 00:09:58 -0700

    delete todo comments and auth to item and user type  query

--- server/api/schema.js
+++ server/api/schema.js
@@ -1,20 +1,11 @@
 const { gql } = require('apollo-server-express');
 
-/**
- *  @TODO: Boomtown Schema
- *
- * Define the types in your GraphQL schema here.
- * For each type, remove the `_: Boolean` placeholder and add the
- * fields as directed. Be sure to finish writing resolvers for all types
- * and any relational fields, where required.
- *
- * We will create the custom Date scalar together.
- */
-
 module.exports = gql`
   scalar Date
 
-  type Item {
+  directive @auth on OBJECT | FIELD_DEFINITION
+
+  type Item @auth {
     id: ID!
     title: String!
     imageurl: String
@@ -25,7 +16,7 @@ module.exports = gql`
     borrower: User
   }
 
-  type User {
+  type User @auth {
     id: ID!
     email: String!
     fullname: String!
