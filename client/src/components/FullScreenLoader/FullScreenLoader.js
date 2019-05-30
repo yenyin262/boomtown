ce0aa4c419d9e1576b3132b18811ae3871de9609
Yen Yin Hing <yenyinf287@gmail.com>
2019-05-30 01:13:54 -0700

    add classes for styling

--- client/src/components/FullScreenLoader/FullScreenLoader.js
+++ client/src/components/FullScreenLoader/FullScreenLoader.js
@@ -3,19 +3,25 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import styles from './styles';
+import NavigationBar from '../NavigationBar';
 
-function Loading(props) {
+function FullScreenLoader(props) {
   const { classes } = props;
   return (
     <div>
-      <CircularProgress className={classes.progress} />
-      <p>“For it is in giving that we receive.” </p>
+      <NavigationBar />
+      <div className={classes.containerLoader}>
+        <div className={classes.loaderImgCaption}>
+          <CircularProgress className={classes.progressCircle} />
+          <p>“For it is in giving that we receive.” </p>
+        </div>
+      </div>
     </div>
   );
 }
 
-Loading.propTypes = {
+FullScreenLoader.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(Loading);
+export default withStyles(styles)(FullScreenLoader);
