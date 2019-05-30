93e8001aaae4f987998ec5a87b603bc26ec64e68
Yen Yin Hing <yenyinf287@gmail.com>
2019-05-30 01:01:39 -0700

    create logic for users if logged in or not

--- client/src/routes/Layout.js
+++ client/src/routes/Layout.js
@@ -4,25 +4,43 @@ import Home from '../pages/Home';
 import Profile from '../pages/Profile';
 import Items from '../pages/Items';
 import Share from '../pages/Share';
+import { ViewerContext } from '../context/ViewerProvider';
 
 export default () => (
-  <Fragment>
-    {/* @TODO: Add your menu component here */
-    /**
-     * @TODO: Define routes here for: /items, /profile, /profile/:userid, and /share
-     *
-     * Provide a wildcard redirect to /items for any undefined route using <Redirect />.
-     *
-     * Later, we'll add logic to send users to one set of routes if they're logged in,
-     * or only view the /welcome page if they are not.
-     */}
-    <Switch>
-      <Route exact path="/items" component={Items} />
-      <Route exact path="/welcome" component={Home} />
-      <Route exact path="/profile" component={Profile} />
-      <Route exact path="/profile/:userid" component={Profile} />
-      <Route path="/share" component={Share} />
-      <Redirect from="*" to="/items" />
-    </Switch>
-  </Fragment>
+  <ViewerContext.Consumer>
+    {({ viewer, loading }) => {
+      if (loading) return null;
+      if (!viewer) {
+        return (
+          <Switch>
+            <Route exact path="/welcome" component={Home} />
+            <Redirect from="*" to="/welcome" />
+          </Switch>
+        );
+      } else {
+        return (
+          <Fragment>
+            <Switch>
+              <Route exact path="/items" component={Items} />
+              <Route exact path="/profile" component={Profile} />
+              <Route exact path="/profile/:userid" component={Profile} />
+              <Route path="/share" component={Share} />
+              <Redirect from="*" to="/items" />
+            </Switch>
+          </Fragment>
+        );
+      }
+    }}
+  </ViewerContext.Consumer>
 );
+{
+  /* @TODO: Add your menu component here */
+  /**
+   * @TODO: Define routes here for: /items, /profile, /profile/:userid, and /share
+   *
+   * Provide a wildcard redirect to /items for any undefined route using <Redirect />.
+   *
+   * Later, we'll add logic to send users to one set of routes if they're logged in,
+   * or only view the /welcome page if they are not.
+   */
+}
