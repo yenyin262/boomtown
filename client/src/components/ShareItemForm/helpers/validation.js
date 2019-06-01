fc39057ec9e6ec6111c05306df3a169bc5c07395
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-01 14:45:40 -0700

    fix validation logic

--- client/src/components/ShareItemForm/helpers/validation.js
+++ client/src/components/ShareItemForm/helpers/validation.js
@@ -1,15 +1,21 @@
 export default function validate(values) {
   const errors = {};
 
-  if (!values.email) {
+  if (!values.title) {
     errors.imageurl = 'Required';
   }
-  if (!values.password) {
+  if (!values.description) {
     errors.title = 'Required';
   }
-  if (values.fullname && !values.fullname) {
+  if (values.tags && !values.tags) {
     errors.description = 'Required';
   }
+
+  /**
+   * @TODO: Write the validation rules for the share form.
+   *
+   * An item title, description, and at least one tag is required for all items.
+   */
 
   return errors;
 }
