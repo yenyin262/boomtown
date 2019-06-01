479e4915226c70b91e06703f45e75087e4155067
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-01 01:41:33 -0700

    delete todos

--- server/config/postgres.js
+++ server/config/postgres.js
@@ -1,18 +1,12 @@
-const { Pool } = require('pg'); // bunch of tunnels to databse  - connection pool / 
+const { Pool } = require('pg'); // bunch of tunnels to databse  - connection pool /
 
-module.exports = (app) => {
-  /**
-   * @TODO: Configuration Variables
-   *
-   *  Retrieve the necessary information to connect to Postgres
-   *  For example: app.get('PG_DB')
-   */
+module.exports = app => {
   return new Pool({
     host: app.get('PG_HOST'),
-    user: app.get('PG_USER'), 
-    password:app.get('PG_PASSWORD'),
-    database:app.get('PG_DB'), 
+    user: app.get('PG_USER'),
+    password: app.get('PG_PASSWORD'),
+    database: app.get('PG_DB'),
     idleTimeoutMillis: 30000, // 30secs
-    connectionTimeoutMillis: 2000  // setting bounds
+    connectionTimeoutMillis: 2000 // setting bounds
   });
 };
