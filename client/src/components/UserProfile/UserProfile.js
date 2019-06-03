b6013b047ed659bf4fe1c07afe2f94c85bb154e6
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-03 00:47:10 -0700

    incorporate details for profilecard

--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -7,33 +7,43 @@ import Gravatar from 'react-gravatar';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
+import { NULL } from 'graphql/language/kinds';
 // import { renderToStringWithData } from 'react-apollo';
 
-function UserProfile({ classes, item, user }) {
-  console.log('dfdf', user);
+function UserProfile({ classes, user }) {
+  console.log('show user props', user);
+  let userBio;
+  if (user.bio === null) {
+    userBio = 'no bio provided';
+  } else {
+    userBio = user.bio;
+  }
   return (
     <Card className={classes.card}>
       <CardContent>
         <div className={classes.userProfileContainer}>
-          <Gravatar
-            className={classes.avatarPic}
-            // email={item.itemowner.email}
-          />
-          <Typography className={classes.title} component="h1" gutterBottom>
-            yenyin262
-            {/* {item.itemowner.fullname} */}
-          </Typography>
+          <div className={classes.picAndUser}>
+            <Gravatar className={classes.avatarPic} email={user.email} />
+            <Typography className={classes.title} component="h1" gutterBottom>
+              {user.fullname}
+            </Typography>
+          </div>
+          <div>
+            <Typography component="h2" className={classes.userInfo}>
+              <span className={classes.itemNo}> {user.items.length}</span> Item
+              shared{' '}
+              <span className={classes.itemNo}>{user.borrowed.length} </span>{' '}
+              Item borrowed
+              {/* {user.items.length <= 1 ? '1 Item Shared' : {user.items.length `Items Shared`}} */}
+            </Typography>
+          </div>
         </div>
-        <Typography component="h2">
-          {/* {item.itemowner.length} Items Shared
-          {item.borrower.length}Items Borrowed */}
-        </Typography>
-        <Typography component="p">
-          {/* {user.bio ? 'No bio provided' : user.bio} */}
+        <Typography component="p" className={classes.biography}>
+          {userBio}
         </Typography>
       </CardContent>
     </Card>
   );
 }
-// export default UserProfile;
+
 export default withStyles(styles)(UserProfile);
