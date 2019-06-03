312fc273a6c9e876c0a51289cbb2df8c98317f71
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-03 00:59:45 -0700

    delete comments

--- server/config/postgres.js
+++ server/config/postgres.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg'); // bunch of tunnels to databse  - connection pool /
+const { Pool } = require('pg');
 
 module.exports = app => {
   return new Pool({
@@ -6,7 +6,7 @@ module.exports = app => {
     user: app.get('PG_USER'),
     password: app.get('PG_PASSWORD'),
     database: app.get('PG_DB'),
-    idleTimeoutMillis: 30000, // 30secs
-    connectionTimeoutMillis: 2000 // setting bounds
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000
   });
 };
