3fc6135b09f2316075ebce9cdddb61cd04e51207
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-03 00:38:27 -0700

    add menucontainer styling

--- client/src/components/MenuOptions/styles.js
+++ client/src/components/MenuOptions/styles.js
@@ -1,11 +1,14 @@
 const styles = theme => ({
   menuIcon: {
-    // margin: '20px 16px 0 0',
     margin: theme.spacing.unit * 2,
     ':focus': {
       outline: 'none'
     }
   },
+  menuContainer: {
+    marginTop: '5px'
+  },
+
   Link: {
     '&:hover': 'none'
   }
