aca6ae7db892ad8d03839d288f935bde0b5b0df5
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-03 00:53:35 -0700

    wrap around viewercontext.consumer

--- client/src/pages/Profile/ProfileContainer.js
+++ client/src/pages/Profile/ProfileContainer.js
@@ -2,22 +2,33 @@ import React, { Component } from 'react';
 import Profile from './Profile';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-// import { Query } from 'react-apollo';
-// import { ALL_USER_ITEMS_QUERY } from '../../apollo/queries';
-// import FullScreenLoader from '../../components/FullScreenLoader';
+import { Query } from 'react-apollo';
+import { ALL_USER_ITEMS_QUERY } from '../../apollo/queries';
+import FullScreenLoader from '../../components/FullScreenLoader';
+import { ViewerContext } from '../../context/ViewerProvider';
 
 class ProfileContainer extends Component {
   render() {
+    const { classes, match } = this.props;
+    const id = match.params.userid;
     return (
-      <Profile />
-      // <Query query={ALL_USER_ITEMS_QUERY}>
-      //   {({ loading, error, data }) => {
-      //     // if (loading) return <FullScreenLoader inverted />;
-      //     if (loading) return <p>Loading...</p>;
-      //     if (error) return <p>{`Error! ${error.message}`}</p>;
-      //     return <Profile classes={this.props.classes} items={data.items} />;
-      //   }}
-      // </Query>
+      <ViewerContext.Consumer>
+        {({ viewer }) => {
+          return (
+            <Query
+              query={ALL_USER_ITEMS_QUERY}
+              variables={{ id: id || viewer.id }}
+            >
+              {({ loading, error, data }) => {
+                if (loading) return <FullScreenLoader inverted />;
+                // if (loading) return <p>Loading...</p>;
+                if (error) return <p>{`Error! ${error.message}`}</p>;
+                return <Profile classes={classes} user={data.user} />;
+              }}
+            </Query>
+          );
+        }}
+      </ViewerContext.Consumer>
     );
   }
 }
