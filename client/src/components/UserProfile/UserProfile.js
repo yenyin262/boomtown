5b9aa13263d748ef43f2a786a65711ae48f72a38
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-03 16:11:19 -0700

    add console.log for testing, add proptype

--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -7,43 +7,54 @@ import Gravatar from 'react-gravatar';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-import { NULL } from 'graphql/language/kinds';
-// import { renderToStringWithData } from 'react-apollo';
 
-function UserProfile({ classes, user }) {
-  console.log('show user props', user);
+function UserProfile({ classes, viewer, itemsofUser }) {
+  console.log('show user props', itemsofUser);
+  console.log(itemsofUser, 'this is items ofuser');
+  console.log(viewer, 'viewer this is me');
   let userBio;
-  if (user.bio === null) {
-    userBio = 'no bio provided';
+  if (viewer.bio === null) {
+    userBio = '"no bio provided."';
   } else {
-    userBio = user.bio;
+    userBio = viewer.bio;
   }
+
   return (
     <Card className={classes.card}>
       <CardContent>
         <div className={classes.userProfileContainer}>
           <div className={classes.picAndUser}>
-            <Gravatar className={classes.avatarPic} email={user.email} />
+            <Gravatar className={classes.avatarPic} email={viewer.email} />
             <Typography className={classes.title} component="h1" gutterBottom>
-              {user.fullname}
+              {itemsofUser.fullname}
             </Typography>
           </div>
           <div>
             <Typography component="h2" className={classes.userInfo}>
-              <span className={classes.itemNo}> {user.items.length}</span> Item
-              shared{' '}
-              <span className={classes.itemNo}>{user.borrowed.length} </span>{' '}
+              <span className={classes.itemNo}>
+                {' '}
+                {itemsofUser.items.length}
+              </span>{' '}
+              Item shared{' '}
+              <span className={classes.itemNo}>
+                {itemsofUser.borrowed.length}{' '}
+              </span>{' '}
               Item borrowed
               {/* {user.items.length <= 1 ? '1 Item Shared' : {user.items.length `Items Shared`}} */}
             </Typography>
+            <Typography component="p" className={classes.biography}>
+              {userBio}
+            </Typography>
           </div>
         </div>
-        <Typography component="p" className={classes.biography}>
-          {userBio}
-        </Typography>
       </CardContent>
     </Card>
   );
 }
 
+UserProfile.propTypes = {
+  classes: PropTypes.object.isRequired,
+  viewer: PropTypes.object,
+  itemsofUser: PropTypes.object
+};
 export default withStyles(styles)(UserProfile);
