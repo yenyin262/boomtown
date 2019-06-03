7a566b12bd63f79c101cd6f4c191c44b362ba339
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-03 16:12:55 -0700

    add proptype

--- client/src/pages/Items/ItemsContainer.js
+++ client/src/pages/Items/ItemsContainer.js
@@ -5,6 +5,7 @@ import styles from './styles';
 // import FullScreenLoader from '../../components/FullScreenLoader';
 import { Query } from 'react-apollo';
 import { ALL_ITEMS_QUERY } from '../../apollo/queries';
+import PropTypes from 'prop-types';
 
 class ItemsContainer extends Component {
   render() {
@@ -22,4 +23,7 @@ class ItemsContainer extends Component {
   }
 }
 
+ItemsContainer.propTypes = {
+  classes: PropTypes.object.isRequired
+};
 export default withStyles(styles)(ItemsContainer);
