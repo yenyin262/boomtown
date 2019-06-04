365bf8750c870af5357e5de294618f57f975cf3e
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-04 16:10:43 -0700

    delete to do and import styles

--- client/src/pages/Share/Share.js
+++ client/src/pages/Share/Share.js
@@ -2,18 +2,8 @@ import React from 'react';
 import ShareItemForm from '../../components/ShareItemForm';
 import ShareItemPreview from '../../components/ShareItemPreview';
 import NavigationBar from '../../components/NavigationBar';
-
-/* 
-  TODO: Create ShareItemFrom and ShareItemPreview in the components dir
-  and call them from this file.
-
-  ShareItemForm is the form that our User will use to add a new item 
-
-  When the user is filling ShareItemForm, we will show a preview of 
-  this item using the ShareItemPreview. 
-  Hint: It should look like any other Item card.
-
-*/
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
 
 const Share = ({ classes, tags }) => {
   return (
@@ -22,12 +12,17 @@ const Share = ({ classes, tags }) => {
         <NavigationBar pageType={null} />
       </div>
 
-      <div>
-        <ShareItemPreview />
-        <ShareItemForm tags={tags} />
+      <div className={classes.container}>
+        <div className={classes.previewCard}>
+          <ShareItemPreview />
+        </div>
+        <div>
+          <ShareItemForm tags={tags} />
+        </div>
       </div>
     </div>
   );
 };
 
-export default Share;
+// export default Share;
+export default withStyles(styles)(Share);
