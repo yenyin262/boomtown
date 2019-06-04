dcfb83095196db2aaadef73eb1eadf08e22390ec
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-04 16:04:08 -0700

    change component to functioanl as no state

--- client/src/components/NavigationBar/NavigationBar.js
+++ client/src/components/NavigationBar/NavigationBar.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+// import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
@@ -12,43 +13,79 @@ import MenuOptions from '../MenuOptions/MenuOptions';
 import { Typography } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
-class NavigationBar extends Component {
-  render() {
-    const { classes } = this.props;
-    let shareLink;
-    if (this.props.pageType != null) {
-      shareLink = (
-        <Link to="/share">
-          <Button className={classes.shareBtn}>
-            <Icon className={classes.icon}>add_circle</Icon>
-            <Typography>Share Something</Typography>
-          </Button>
-        </Link>
-      );
-    } else {
-      shareLink = <div />;
-    }
-    return (
-      <AppBar position="static" className={classes.navContainer}>
-        <Toolbar>
-          <Link to="/items">
-            <IconButton>
-              <img
-                className={classes.bmtownImg}
-                src={bmtwnLogo}
-                alt="boomtown_logo"
-              />
-            </IconButton>
-          </Link>{' '}
-          <div className={classes.childContainer}>
-            {shareLink}
-            <MenuOptions />
-          </div>
-        </Toolbar>
-      </AppBar>
+const NavigationBar = ({ classes, pageType }) => {
+  let shareLink;
+  if (pageType != null) {
+    shareLink = (
+      <Link to="/share">
+        <Button className={classes.shareBtn}>
+          <Icon className={classes.icon}>add_circle</Icon>
+          <Typography>Share Something</Typography>
+        </Button>
+      </Link>
     );
+  } else {
+    shareLink = <div />;
   }
-}
+
+  return (
+    <AppBar position="static" className={classes.navContainer}>
+      <Toolbar>
+        <Link to="/items">
+          <IconButton>
+            <img
+              className={classes.bmtownImg}
+              src={bmtwnLogo}
+              alt="boomtown_logo"
+            />
+          </IconButton>
+        </Link>{' '}
+        <div className={classes.childContainer}>
+          {shareLink}
+          <MenuOptions />
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+// class NavigationBar extends Component {
+//   render() {
+//     const { classes } = this.props;
+//     let shareLink;
+//     if (this.props.pageType != null) {
+//       shareLink = (
+//         <Link to="/share">
+//           <Button className={classes.shareBtn}>
+//             <Icon className={classes.icon}>add_circle</Icon>
+//             <Typography>Share Something</Typography>
+//           </Button>
+//         </Link>
+//       );
+//     } else {
+//       shareLink = <div />;
+//     }
+//     return (
+//       <AppBar position="static" className={classes.navContainer}>
+//         <Toolbar>
+//           <Link to="/items">
+//             <IconButton>
+//               <img
+//                 className={classes.bmtownImg}
+//                 src={bmtwnLogo}
+//                 alt="boomtown_logo"
+//               />
+//             </IconButton>
+//           </Link>{' '}
+//           <div className={classes.childContainer}>
+//             {shareLink}
+//             <MenuOptions />
+//           </div>
+//         </Toolbar>
+//       </AppBar>
+//     );
+//   }
+// }
 
 NavigationBar.propTypes = {
   classes: PropTypes.object.isRequired,
