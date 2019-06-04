86c7c1bd842d23f9e6251fb7ae1845cb452becf6
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-04 16:07:55 -0700

    incorporate fullscreenloader

--- client/src/pages/Items/ItemsContainer.js
+++ client/src/pages/Items/ItemsContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Items from './Items';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-// import FullScreenLoader from '../../components/FullScreenLoader';
+import FullScreenLoader from '../../components/FullScreenLoader';
 import { Query } from 'react-apollo';
 import { ALL_ITEMS_QUERY } from '../../apollo/queries';
 import PropTypes from 'prop-types';
@@ -12,9 +12,7 @@ class ItemsContainer extends Component {
     return (
       <Query query={ALL_ITEMS_QUERY}>
         {({ loading, error, data }) => {
-          // if (loading) return <FullScreenLoader inverted />;
-          // console.log('my_data', data.items);
-          if (loading) return <p>Loading...</p>;
+          if (loading) return <FullScreenLoader inverted />;
           if (error) return <p>{`Error! ${error.message}`}</p>;
           return <Items classes={this.props.classes} items={data.items} />;
         }}
