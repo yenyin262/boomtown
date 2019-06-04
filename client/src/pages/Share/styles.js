39b8128592541e358a0c1085a56389aabeba01b6
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-04 16:12:12 -0700

    incorporate style for share page

--- client/src/pages/Share/styles.js
+++ client/src/pages/Share/styles.js
@@ -1,3 +1,14 @@
-const styles = theme => ({});
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    padding: '160px',
+    paddingTop: '60px'
+  },
+  previewCard: {
+    width: '27vw',
+    display: 'block'
+  }
+});
 
 export default styles;
