23dfc10ff80bfdc749a71cdd36c67e85f32d4b70
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-04 01:02:52 -0700

    add console.log for testing and uncomment mapstatetoprops

--- client/src/components/ShareItemPreview/ShareItemPreview.js
+++ client/src/components/ShareItemPreview/ShareItemPreview.js
@@ -4,16 +4,18 @@ import { connect } from 'react-redux';
 import { ViewerContext } from '../../context/ViewerProvider';
 
 const ShareItemPreview = ({ shareItemPreview }) => {
+  console.log(shareItemPreview);
   return (
     <ViewerContext.Consumer>
       {({ viewer }) => {
+        console.log(viewer, 'doed');
         return <CardItem item={shareItemPreview} viewer={viewer} />;
       }}
     </ViewerContext.Consumer>
   );
 };
 
-// const mapStatetoProps = ({ shareItemPreview }) => ({ shareItemPreview });
+const mapStatetoProps = ({ shareItemPreview }) => ({ shareItemPreview });
 
 // const mapStatetoProps = state => {
 //   return { ...state };
