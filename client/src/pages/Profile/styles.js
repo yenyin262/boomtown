4bf051c9c9116f8c1ddea1fa244bc0a07deeec10
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-06 13:09:17 -0700

    incorporate styling

--- client/src/pages/Profile/styles.js
+++ client/src/pages/Profile/styles.js
@@ -3,6 +3,21 @@ const styles = theme => ({
     padding: '80px',
     backgroundColor: 'black',
     height: '100vh'
+  },
+
+  sharedHeader: {
+    color: '#f9a825',
+    fontSize: '37px',
+    fontWeight: '400',
+    fontFamily: ' Roboto'
+  },
+
+  profileGrid: {
+    width: '30%'
+  },
+
+  body: {
+    backgroundColor: 'black'
   }
 });
 
