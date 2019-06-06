5f43cd3a498c142464fa7b5cfea4e7b2461b9635
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-06 13:02:30 -0700

    comment out unnecessary code testing

--- client/src/components/CardItem/styles.js
+++ client/src/components/CardItem/styles.js
@@ -1,15 +1,19 @@
 const styles = {
   card: {
-    minWidth: '200px',
-    width: '400px',
-    margin: '20px auto'
+    // minWidth: '200px',
+    // width: '400px',
+    margin: '10px 10px'
+    // margin: '16px 16px'
   },
   media: {
-    height: 300
+    // height: 300
+    height: '0',
+    paddingTop: '56.25%'
   },
 
   mainCard: {
-    height: '560px'
+    height: '522px',
+    width: '421px'
   },
 
   containerProfile: {
@@ -17,23 +21,35 @@ const styles = {
     width: '52%',
     justifyContent: 'space-evenly',
     alignItems: 'center',
-    marginTop: '10px'
+    // marginTop: '10px'
+    paddingLeft: '24px',
+    paddingRight: '24px',
+    paddingTop: '16px',
+    paddingBottom: '16px'
   },
   titleName: {
     fontSize: '24px',
-    fontWeight: '400'
+    fontWeight: '400',
+    marginBottom: '10px'
   },
   tagName: {
     fontSize: '14px',
-    fontWeight: 400
+    fontWeight: 400,
+    marginBottom: '5px'
   },
   descriptionName: {
     fontSize: '16px',
-    fontWeight: 400
+    fontWeight: 400,
+    marginBottom: '5px'
   },
   button: {
     fontSize: '16px',
-    padding: '8px 24px'
+    padding: '8px 24px',
+    minHeight: '40px'
+  },
+  buttonText: {
+    fontSize: '16px',
+    fontWeight: 400
   },
 
   avatarPic: {
