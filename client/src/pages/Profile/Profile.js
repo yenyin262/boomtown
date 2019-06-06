f5fb6b60db7b29058bbe2d6ab92854068bf7d1c8
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-06 13:08:17 -0700

    add classes, user props

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -5,19 +5,27 @@ import styles from './styles';
 import NavigationBar from '../../components/NavigationBar';
 import CardGrid from '../../components/CardGrid';
 
-const Profile = ({ classes, itemsofUser, viewer }) => {
-  console.log('print users id', itemsofUser);
-  console.log(viewer, 'view me ');
+const Profile = ({ classes, itemsofUser, user }) => {
+  console.log('print users item', itemsofUser);
+  console.log(user, 'no');
+  // console.log(viewer, 'view me ');
   return (
     <div>
       <NavigationBar pageType={true} />
       <div className={classes.profileContainer}>
         <UserProfile
-          key={viewer.index}
+          // key={viewer.index}
           itemsofUser={itemsofUser}
-          viewer={viewer}
+          // viewer={viewer}
+          user={user}
         />
-        <CardGrid itemsofUser={itemsofUser} />
+
+        <div>
+          <h1 className={classes.sharedHeader}>Shared Items</h1>
+        </div>
+        <div className={classes.profileGrid}>
+          <CardGrid items={itemsofUser} />
+        </div>
       </div>
     </div>
   );
