6f48a31ab5dcafcb0387cff4e387c0f4aa7bb1c9
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-07 01:17:16 -0700

    insert variable tags and extra div

--- client/src/components/CardItem/CardItem.js
+++ client/src/components/CardItem/CardItem.js
@@ -1,5 +1,5 @@
-import React from 'react';
-// import React, { Component } from 'react';
+// import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -13,15 +13,21 @@ import { Link } from 'react-router-dom';
 import styles from './styles';
 import Gravatar from 'react-gravatar';
 
-const CardItem = ({ classes, item, viewer }) => {
+const CardItem = ({ classes, item, viewer, updatedItem }) => {
   let getTheTags;
   let a = item;
   let b = item.tags;
   let c = item.tags[0];
+  let tags = item.tags.length > 0 ? item.tags.join(', ') : item.tags;
 
+  console.log(item);
+  console.log(updatedItem);
   if (a != null && b != null && c != null) {
+    console.log('Card Item true');
     getTheTags = item.tags[0].title;
+    console.log(item.tags[0].title, 'tags item');
   } else {
+    console.log('Card Item false');
     getTheTags = '';
   }
 
@@ -31,63 +37,65 @@ const CardItem = ({ classes, item, viewer }) => {
   // } else {
   //   getItemOwner = { viewer };
   // }
-
+  console.log('card item', item);
   return (
     <Link className={classes.card} to={`/profile/${item.itemowner.id}`}>
-      <Card className={classes.mainCard}>
-        <CardMedia
-          className={classes.media}
-          image={
-            item.imageurl
-              ? item.imageurl
-              : 'http://via.placeholder.com/350x250?text=Please+select+an+image'
-          }
-        />
-        <div>
-          <div className={classes.containerProfile}>
-            <Gravatar
-              className={classes.avatarPic}
-              email={item.itemowner.email || viewer.email}
-            />
-            <div>
-              <Typography component="p">{item.itemowner.fullname}</Typography>
-              <Typography
-                component="p"
-                color="textSecondary"
-                gutterBottom
-                className={classes.itemCreated}
-              >
-                {moment(item.created)
-                  .startOf('day')
-                  .fromNow()}
-              </Typography>
+      <div className={classes.mainContainerCard}>
+        <Card className={classes.mainCard}>
+          <CardMedia
+            className={classes.media}
+            image={
+              item.imageurl
+                ? item.imageurl
+                : 'http://via.placeholder.com/350x250?text=Please+select+an+image'
+            }
+          />
+          <div>
+            <div className={classes.containerProfile}>
+              <Gravatar
+                className={classes.avatarPic}
+                email={item.itemowner.email || viewer.email}
+              />
+              <div>
+                <Typography component="p">{item.itemowner.fullname}</Typography>
+                <Typography
+                  component="p"
+                  color="textSecondary"
+                  gutterBottom
+                  className={classes.itemCreated}
+                >
+                  {moment(item.created)
+                    .startOf('day')
+                    .fromNow()}
+                </Typography>
+              </div>
             </div>
           </div>
-        </div>
-        <CardContent>
-          <Typography component="h2" className={classes.titleName}>
-            {item.title}
-          </Typography>
-          <Typography
-            component="p"
-            color="textSecondary"
-            className={classes.tagName}
-          >
-            {getTheTags}
-          </Typography>
-
-          <Typography component="h3" className={classes.descriptionName}>
-            {item.description}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button variant="outlined" className={classes.button}>
-            <Typography component="h3" className={classes.buttonText}>
-              Borrow
+          <CardContent>
+            <Typography component="h2" className={classes.titleName}>
+              {item.title}
             </Typography>
-          </Button>
-        </CardActions>
-      </Card>
+            <Typography
+              component="p"
+              color="textSecondary"
+              className={classes.tagName}
+            >
+              {getTheTags ? getTheTags : tags}
+            </Typography>
+
+            <Typography component="h3" className={classes.descriptionName}>
+              {item.description}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button variant="outlined" className={classes.button}>
+              <Typography component="h3" className={classes.buttonText}>
+                Borrow
+              </Typography>
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
     </Link>
   );
 };
@@ -98,6 +106,7 @@ const CardItem = ({ classes, item, viewer }) => {
 //     const { item } = this.props;
 //     console.log('This is the props', this.props);
 //     console.log('This is the second one', item);
+//     let tags = item.tags.length > 0 ? item.tags.join(', ') : item.tags;
 
 //     let getTheTags;
 //     let a = item;
@@ -109,6 +118,7 @@ const CardItem = ({ classes, item, viewer }) => {
 //     } else {
 //       getTheTags = '';
 //     }
+
 //     return (
 //       <Link className={classes.card} to={`/profile/${item.itemowner.id}`}>
 //         <Card className={classes.mainCard}>
@@ -154,7 +164,7 @@ const CardItem = ({ classes, item, viewer }) => {
 //               color="textSecondary"
 //               className={classes.tagName}
 //             >
-//               {getTheTags}
+//               {getTheTags ? getTheTags : tags}
 //             </Typography>
 
 //             <Typography component="h3" className={classes.descriptionName}>
@@ -173,6 +183,7 @@ const CardItem = ({ classes, item, viewer }) => {
 //     );
 //   }
 // }
+
 CardItem.propTypes = {
   classes: PropTypes.object.isRequired,
   item: PropTypes.object
