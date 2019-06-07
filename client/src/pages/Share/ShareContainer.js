5b3c5cb9dc5c35c524c5fa00d1a8b746b8470dcf
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-07 01:23:34 -0700

    add comment code for temp testing

--- client/src/pages/Share/ShareContainer.js
+++ client/src/pages/Share/ShareContainer.js
@@ -9,13 +9,17 @@ import PropTypes from 'prop-types';
 
 class ShareContainer extends Component {
   render() {
+    // const { match } = this.props;
+
+    // const id = match.params.userid;
+    // variables={{ id: id || viewer.id }}
     return (
       // <Query query={ALL_TAGS_QUERY}  fetchPolicy="network-only">
       <Query query={ALL_TAGS_QUERY}>
         {({ loading, error, data }) => {
           if (loading) return <FullScreenLoader inverted />;
           if (error) return <p>{`Error! ${error.message}`}</p>;
-          console.log(data, 'data');
+          console.log(data, 'data in share ');
           return <Share classes={this.props.classes} tags={data.tags} />;
         }}
       </Query>
