c0221b12319e39f7527f031cb113d65e74fbcad6
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-08 00:47:56 -0700

    delete console.logs and commented code

--- client/src/components/CardGrid/CardGrid.js
+++ client/src/components/CardGrid/CardGrid.js
@@ -1,71 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import CardItem from '../CardItem';
 import styles from './styles';
 
-const CardGrid = ({ classes, items }) => {
-  console.log('This is an item', items);
-
+const CardGrid = ({ classes, items, viewer }) => {
   let getItems;
 
   let a = items;
-  // let b = items.map(item);
-  // let c = item;
 
   if (a != null) {
-    getItems = items.map(item => <CardItem item={item} key={item.id} />);
+    getItems = items.map(item => (
+      <CardItem item={item} key={item.id} viewer={viewer} />
+    ));
   } else {
     getItems = '';
   }
 
-  console.log('This is the first one', items);
-
   return (
     <Grid container alignContent="center" spacing={16}>
       <Grid item xs={12}>
-        <Grid container className={classes.containerGrid}>
-          {/* {items.map(item => <CardItem item={item} key={item.id} />)} */}
+        <Grid container className={classes.containerGrid} viewer={viewer}>
           {getItems}
         </Grid>
       </Grid>
     </Grid>
   );
 };
-
-// class CardGrid extends Component {
-//   // state = {
-//   //   spacing: '16'
-//   // };
-
-//   render() {
-//     const { items } = this.props;
-//     let getItems;
-
-//     let a = items;
-//     // let b = items.map(item);
-//     // let c = item;
-
-//     if (a != null) {
-//       getItems = items.map(item => <CardItem item={item} key={item.id} />);
-//     } else {
-//       getItems = '';
-//     }
-
-//     console.log('This is the first one', items);
-//     return (
-//       <Grid container alignContent="center" spacing={16}>
-//         <Grid item xs={12}>
-//           <Grid container>
-//             {/* {items.map(item => <CardItem item={item} key={item.id} />)} */}
-//             {getItems}
-//           </Grid>
-//         </Grid>
-//       </Grid>
-//     );
-//   }
-// }
 
 CardGrid.propTypes = {
   classes: PropTypes.object.isRequired
