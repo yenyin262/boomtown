b03c920fca6d9b2e67f68846c72e257587b3a211
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-08 00:51:57 -0700

    delete console.logs and commented code

--- client/src/components/ShareItemPreview/ShareItemPreview.js
+++ client/src/components/ShareItemPreview/ShareItemPreview.js
@@ -3,14 +3,10 @@ import CardItem from '../CardItem';
 import { connect } from 'react-redux';
 import { ViewerContext } from '../../context/ViewerProvider';
 
-const ShareItemPreview = ({ shareItemPreview, tags }) => {
-  console.log('this is sahre item preview', shareItemPreview);
-  console.log('share item render', tags);
-
+const ShareItemPreview = ({ shareItemPreview }) => {
   return (
     <ViewerContext.Consumer>
       {({ viewer }) => {
-        console.log(viewer, 'doed');
         return <CardItem item={shareItemPreview} viewer={viewer} />;
       }}
     </ViewerContext.Consumer>
@@ -19,7 +15,4 @@ const ShareItemPreview = ({ shareItemPreview, tags }) => {
 
 const mapStatetoProps = ({ shareItemPreview }) => ({ shareItemPreview });
 
-// const mapStatetoProps = state => {
-//   return { ...state };
-// };
 export default connect(mapStatetoProps)(ShareItemPreview);
