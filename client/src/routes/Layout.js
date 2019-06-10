5701c58ab048c741d409f6cd071b2843559f3c70
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-09 20:27:05 -0700

    deleted comments

--- client/src/routes/Layout.js
+++ client/src/routes/Layout.js
@@ -33,14 +33,3 @@ export default () => (
     }}
   </ViewerContext.Consumer>
 );
-{
-  /* @TODO: Add your menu component here */
-  /**
-   * @TODO: Define routes here for: /items, /profile, /profile/:userid, and /share
-   *
-   * Provide a wildcard redirect to /items for any undefined route using <Redirect />.
-   *
-   * Later, we'll add logic to send users to one set of routes if they're logged in,
-   * or only view the /welcome page if they are not.
-   */
-}
