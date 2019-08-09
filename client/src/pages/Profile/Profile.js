df0ba39a0d5ee2c72373c6d8020ccf8b3ff17543
Yen Yin Hing <yenyinf287@gmail.com>
2019-08-08 23:57:19 -0700

    add proptype and add showShareIcon variable name change

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -4,14 +4,14 @@ import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
 import NavigationBar from '../../components/NavigationBar';
 import CardGrid from '../../components/CardGrid';
+import PropTypes from 'prop-types';
 
 const Profile = ({ classes, itemsofUser, user }) => {
   return (
     <div>
-      <NavigationBar pageType={true} />
+      <NavigationBar showShareIcon={true} />
       <div className={classes.profileContainer}>
         <UserProfile itemsofUser={itemsofUser} user={user} />
-
         <div>
           <h1 className={classes.sharedHeader}>Shared Items</h1>
         </div>
@@ -21,6 +21,12 @@ const Profile = ({ classes, itemsofUser, user }) => {
       </div>
     </div>
   );
+};
+
+Profile.propTypes = {
+  classes: PropTypes.object.isRequired,
+  itemsofUser: PropTypes.array,
+  user: PropTypes.object
 };
 
 export default withStyles(styles)(Profile);
