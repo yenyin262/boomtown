ceb46503d4d81f55842f585e669976fc26357c28
Yen Yin Hing <yenyinf287@gmail.com>
2019-08-08 23:59:38 -0700

    incorporate minheight for profilecontainer

--- client/src/pages/Profile/styles.js
+++ client/src/pages/Profile/styles.js
@@ -2,22 +2,24 @@ const styles = theme => ({
   profileContainer: {
     padding: '80px',
     backgroundColor: 'black',
-    height: '100%'
+    height: '100%',
+    minHeight: '100vh'
   },
-
   sharedHeader: {
     color: '#f9a825',
     fontSize: '37px',
     fontWeight: '400',
     fontFamily: ' Roboto'
   },
-
   profileGrid: {
     display: 'flex'
   },
-
   body: {
     backgroundColor: 'black'
+  },
+  error: {
+    marginLeft: '20px',
+    fontSize: '20px'
   }
 });
 
