ee11e481f1013c1030e2008761b2d8a4a10f502a
Yen Yin Hing <yenyinf287@gmail.com>
2019-08-08 23:58:56 -0700

    change to stateless component and add 404 error page

--- client/src/pages/Profile/ProfileContainer.js
+++ client/src/pages/Profile/ProfileContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Profile from './Profile';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
@@ -8,24 +8,29 @@ import FullScreenLoader from '../../components/FullScreenLoader';
 import { ViewerContext } from '../../context/ViewerProvider';
 import PropTypes from 'prop-types';
 
-class ProfileContainer extends Component {
-  render() {
-    const { classes, match } = this.props;
+const NoMatchPage = ({ classes }) => {
+  return <h3 className={classes.error}>404 - Page Not found</h3>;
+};
 
-    const id = match.params.userid;
-    return (
-      <ViewerContext.Consumer>
-        {({ viewer }) => {
-          return (
-            <Query
-              query={ALL_USER_ITEMS_QUERY}
-              variables={{ id: id || viewer.id }}
-              fetchPolicy="network-only"
-            >
-              {({ loading, error, data }) => {
-                if (loading) return <FullScreenLoader inverted />;
-                if (error) return <p>{`Error! ${error.message}`}</p>;
-                return (
+const ProfileContainer = ({ classes, match }) => {
+  const id = match.params.userid;
+  return (
+    <ViewerContext.Consumer>
+      {({ viewer }) => {
+        return (
+          <Query
+            query={ALL_USER_ITEMS_QUERY}
+            variables={{ id: id || viewer.id }}
+            fetchPolicy="network-only"
+          >
+            {({ loading, error, data }) => {
+              if (loading) return <FullScreenLoader inverted />;
+              if (error) return <p>{`Error! ${error.message}`}</p>;
+              let GetUserProfile;
+              if (!data.user) {
+                GetUserProfile = <NoMatchPage classes={classes} />;
+              } else {
+                GetUserProfile = (
                   <Profile
                     classes={classes}
                     itemsofUser={data.user.items}
@@ -33,14 +38,16 @@ class ProfileContainer extends Component {
                     viewer={viewer}
                   />
                 );
-              }}
-            </Query>
-          );
-        }}
-      </ViewerContext.Consumer>
-    );
-  }
-}
+              }
+              return <div>{GetUserProfile}</div>;
+            }}
+          </Query>
+        );
+      }}
+    </ViewerContext.Consumer>
+  );
+};
+
 ProfileContainer.propTypes = {
   classes: PropTypes.object.isRequired
 };
